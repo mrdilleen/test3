8f419f5976378dd442e4d097ade3cb716656ce3d
mrdilleen <mrdilleen@protonmail.com>
2026-01-28 15:01:02 +0000

    Add files via upload

---
+++ 15 10 25 first years.py
@@ -0,0 +1,11 @@
+#if elif else
+#Check if letter is a, b, c or different
+letter = 'x'
+if (letter == 'a'):
+    print('letter is a')
+elif (letter == 'b'):
+    print('letter is b')
+elif (letter == 'c'):
+    print('letter is c')
+else:
+    print("I don't recognise the letter")
